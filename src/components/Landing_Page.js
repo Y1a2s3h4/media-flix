5184de961a6cac9426608cd7628f04f31d64aaa1
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-02-26 18:28:08 +0530

    small changes...

--- src/components/Landing_Page.js
+++ src/components/Landing_Page.js
@@ -45,32 +45,48 @@ export default class Landing_Page extends Component {
               onChange={this.onChange}
               required="True"
             />
-            <button type="submit" className="btn search btn-outline-primary btn-block">
+            <button
+              type="submit"
+              className="btn search btn-outline-primary btn-block"
+            >
               Search
             </button>
           </form>
         </div>
-        <div id="main-card">
+        <div className="row main-card">
           {this.state.data.map(item => {
             return (
-              <div key={item.id} class="card mb-3" style={{ maxWidth: "35rem" }}>
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img
-                      src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-                      class="card-img"
-                      alt="..."
-                    />
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">{item.title}</h5>
-                      <p class="card-text">Release Date: {item.release_date}</p>
-                      <p class="card-text">Original Language: {item.original_language}</p>
-                      <p class="card-text">Ratings: {item.vote_average}</p>
-                      <Link to={`/${item.id}`} className="btn more-info btn-outline-primary">
-                        More Info
-                      </Link>
+              <div className="col-sm-12 col-md-6 col-lg-4">
+                <div
+                  key={item.id}
+                  class="card mb-3"
+                  style={{ maxWidth: "35rem" }}
+                >
+                  <div class="row no-gutters">
+                    <div class="col-md-4">
+                      <img
+                        src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                        class="card-img"
+                        alt="..."
+                      />
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">{item.title}</h5>
+                        <p class="card-text">
+                          Release Date: {item.release_date}
+                        </p>
+                        <p class="card-text">
+                          Original Language: {item.original_language}
+                        </p>
+                        <p class="card-text">Ratings: {item.vote_average}</p>
+                        <Link
+                          to={`/${item.id}`}
+                          className="btn more-info btn-outline-primary"
+                        >
+                          More Info
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -17,39 +17,50 @@ export default function Popular() {
       });
   };
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      <div id="main-card">
-        {movie_data.map(item => (
-          <div key={item.id} class="card mb-3" style={{ maxWidth: "35rem" }}>
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img
-                  src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-                  class="card-img"
-                  alt="..."
-                />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">{item.title}</h5>
-                  <p class="card-text">Release Date: {item.release_date}</p>
-                  <p class="card-text">
-                    Original Language: {item.original_language}
-                  </p>
-                  <p class="card-text">Ratings: {item.vote_average}</p>
-                  <Link
-                    to={`/${item.id}`}
-                    className="btn more-info btn-outline-primary"
-                  >
-                    More Info
-                  </Link>
+    <div className="c-marginTop">
+      <center>
+        <h1 className="text-white d-inline text-uppercase c-title">
+          Popular Movies
+        </h1>
+        <div className="row main-card">
+          {movie_data.map(item => (
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <div
+                key={item.id}
+                class="card mb-3"
+                style={{ maxWidth: "35rem" }}
+              >
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img
+                      src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                      class="card-img"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body text-left">
+                      <h5 class="card-title title">{item.title}</h5>
+                      <p class="card-text">Release Date: {item.release_date}</p>
+                      <p class="card-text">
+                        Original Language: {item.original_language}
+                      </p>
+                      <p class="card-text">Ratings: {item.vote_average}</p>
+                      <Link
+                        to={`/${item.id}`}
+                        className="btn more-info btn-outline-primary"
+                      >
+                        More Info
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </center>
+
       <div className="bg-header "></div>
     </div>
   );
